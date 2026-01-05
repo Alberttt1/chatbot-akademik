29e30560c0c51ea0b8ec19bc86a0a5bff90ccf9d
Alberttt1 <albertsuwartono9@gmail.com>
2026-01-05 14:24:42 +0700

    ready for railway deploy

--- app.py
+++ app.py
@@ -189,5 +189,8 @@ def chat():
     return render_template("index.html")
 
 
+import os
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
